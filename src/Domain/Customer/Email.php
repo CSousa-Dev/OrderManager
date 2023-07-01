d9b6f7cd3ab861e9451b6702181a3afa145088c9
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2023-07-01 00:27:30 -0300

    Adicionando trait de validação na classe Email

--- src/Domain/Customer/Email.php
+++ src/Domain/Customer/Email.php
@@ -2,14 +2,18 @@
 
 namespace OrderManager\Domain\Customer;
 
+use OrderManager\Domain\Helpers\DataValidator;
+
 class Email 
 {
   private string $address;
+  use DataValidator;
+
   public function __construct(string $address)
   {
     $this->validateMailAddress($address);
-    $this->checkIfEmailIsTooShort($address);
-    $this->checkIfEmailIsTooLong($address);
+    $this->throwExceptionIfStringValueLessThan($address, 8, 'O endereço de e-mail informado é muito curto!');
+    $this->throwExceptionIfStringValueGreaterThan($address, 55, 'O endereço de e-mail informado é muito longo!');
 
     $this->address = $address;
   }
@@ -17,28 +21,9 @@ class Email
   private function validateMailAddress($address)
   {
     if(filter_var($address, FILTER_VALIDATE_EMAIL) == false){
-      throw new \InvalidArgumentException('O endereço de e-mail informado não está completo, informe um endereço de e-mail válido!');
+      throw new \DomainException('O endereço de e-mail informado não está completo, informe um endereço de e-mail válido!');
     }
   }
-
-  private function checkIfEmailIsTooShort(string $address)
-  {
-    $numberOfLetters = strlen($address);
-    if($numberOfLetters < 8)
-    {
-      throw new \InvalidArgumentException('O endereço de e-mail informado é muito curto!');
-    }
-  }
-
-  public function checkIfEmailIsTooLong(string $address)
-  {
-    $numberOfLetters = strlen($address);
-    if($numberOfLetters > 55)
-    {
-      throw new \InvalidArgumentException('O endereço de e-mail informado é muito longo!');
-    }
-  }
-
 
   public function __toString()
   {
